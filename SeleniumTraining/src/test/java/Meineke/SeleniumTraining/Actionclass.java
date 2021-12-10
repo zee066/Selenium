15d735168362180bdad18842327850009d3e62e2
15408 <15408@10.0.0.235>
2021-12-09 21:34:37 -0500

    The first repository

---
+++ SeleniumTraining/src/test/java/Meineke/SeleniumTraining/Actionclass.java
@@ -0,0 +1,32 @@
+package Meineke.SeleniumTraining;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Actionclass {
+public static WebDriver drivers;
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\15408\\git\\repository\\SeleniumTraining\\src\\Drivers\\chromedriver.exe");
+drivers=new ChromeDriver();
+drivers.navigate().to("https://amazon.com");
+
+drivers.manage().window().maximize();
+
+WebElement sign=drivers.findElement(By.xpath("//*[@id='nav-link-accountList']"));
+Actions action=new Actions(drivers);
+action.moveToElement(sign).build().perform();
+drivers.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+WebElement kindle=drivers.findElement(By.linkText("Music Library"));
+action.moveToElement(kindle).click().build().perform();
+drivers.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+drivers.close();
+	}
+
+}
